61003d5866295b445181570eab60b42b1db03066
Gayatri Vadaparty <67108072+GayatriVadaparty@users.noreply.github.com>
2023-07-08 23:55:45 +0530

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.get('/metals', (req, res) => {
+  fs.readFile('nested_data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.get('/metals/:metalName', (req, res) => {
+    const metalName = req.params.metalName;
+    fs.readFile('nested_data.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+      const metals = JSON.parse(data);
+      const metal = metals[metalName];
+      if (metal) {
+        res.json(metal);
+      } else {
+        res.status(404).json({ error: 'Metal not found' });
+      }
+    });
+  });
+  
+  
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
